69f629394799da1f96a671a5c59f21dd8b05678b
Tobi <467bnguyen@gmail.com>
2020-09-22 13:00:38 -0700

    added temporary login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Importing Pages
+import BusinessProfile from './pages/BuisnessProfile'
+import BusinessView from './pages/BuisnessView'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile'
+
+// Import Nav
+import Nav from './components/Nav'
 
 const App = () => {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/businessprofile" component={BusinessProfile} />
+          <Route path="/businessview" component={BusinessView} />
+          <Route path="/login" component={Login} />
+          <Route path="/userprofile" component={UserProfile} />
+        </Switch>
+      </Router>
+    </>
   )
 }
 
