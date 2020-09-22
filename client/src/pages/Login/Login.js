cabbe565993f10b3eb19a38889d340a5d026b204
Tobi <467bnguyen@gmail.com>
2020-09-22 13:21:09 -0700

    added signup on login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -7,12 +7,22 @@ function Login() {
     //   }
     return (
         <div className="login-page">
-            <form className="box">
+            <form className="login-form">
                 <h1>Login</h1>
                 <input type="text" name="userName" placeholder="User Name" />
                 <input type="password" name="password" placeholder="Password" />
                 <input type="submit" name="submit" value="Login" />
             </form>
+
+            <form className="signup-form">
+                <h1>Sign Up</h1>
+                <input type="text" name="userName" placeholder="First Name" />
+                <input type="text" name="userName" placeholder="Last Name" />
+                <input type="password" name="password" placeholder="Password" />
+                <input type="text" name="userName" placeholder="Email" />
+                <input type="submit" name="submit" value="Submit" />
+            </form>
+
         </div>
     )
 }
