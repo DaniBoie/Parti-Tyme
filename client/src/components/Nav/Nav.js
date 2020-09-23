c99ba5aecc93059ff4292a7e66c2e62d6638814e
Tobi <467bnguyen@gmail.com>
2020-09-23 00:25:58 -0700

    testing hamburger icon

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
+import './Nav.css'
 
 const Nav = () => {
+
+  // const [showItems, setShowItems] = useState({
+  //   change: ""
+  // })
+  // function handleMenuBtn() {
+  //   if (showItems.change === "show")
+  //   {
+  //     setShowItems({change: ""})
+  //     console.log(showItems.change)
+  //   }
+  //   else {
+  //     setShowItems({change: "show"})
+  //     console.log(showItems.change)
+  //   }
+  // }
+
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/businessprofile">BusinessProfile</Link>
-      <Link to="/businessview">BusinessView</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/userprofile">UserProfile</Link>
-    </>
+    <nav className= "nav-bar">
+      <div className="menu-button" onClick={handleMenuBtn}>
+        <div className="button-line"></div>
+        <div className="button-line"></div>
+        <div className="button-line"></div>
+      </div>
+
+      <form className = "navForm">
+        <input className = "navInput" type="text" placeholder="Search ..."/>
+        <button className="search-button"><i className="fa fa-search"></i></button>
+      </form>
+
+      
+
+      {/* <div className="menu-button" onClick={handleMenuBtn}>
+        <div className="button-line"></div>
+        <div className="button-line"></div>
+        <div className="button-line"></div>
+      </div>
+
+
+      <div className={`nav-items ${showItems.change}`}>
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/businessprofile" className="nav-link">BusinessProfile</Link>
+        <Link to="/businessview" className= "nav-link">BusinessView</Link>
+        <Link to="/login" className= "nav-link">Login</Link>
+        <Link to="/userprofile" className= "nav-link">UserProfile</Link>
+      </div>
+
+      <button className="logButton">Login</button> */}
+
+    </nav>
   )
 }
 
 export default Nav
+
