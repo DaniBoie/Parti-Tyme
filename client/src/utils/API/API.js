39b94347c5b0687c1f96a0daff86dc1ef85be447
Bao Nguyen <467bnguyen@gmail.com>
2020-09-25 10:07:22 -0700

    layout for login page is finsihed

--- client/src/utils/API/API.js
+++ client/src/utils/API/API.js
@@ -1,25 +1,27 @@
-import axios from 'axios'
+import axios from "axios";
 
 const API = {
   // **USER API CALLS**
+
   // Send Frontend data as object
   registerUser: (data) => axios.post(`/api/users/register`, data),
-  loginUser: (data) => axios.post('/api/users/login', data),
-  getUser: () => axios.get('/api/users/me'),
+  loginUser: (data) => axios.post("/api/users/login", data),
+  getUser: () => axios.get("/api/users/me"),
 
   // **USER PROFILE API CALLS**
   createSettings: (data) => axios.post(`/api/settings`, data),
-  updateSettings: (data) => axios.put('/api/settings', data),
+  updateSettings: (data) => axios.put("/api/settings", data),
 
   // **BUISNESS API CALLS**
   createBusiness: (data) => axios.post(`/api/buisness`, data),
-  updateBusiness: (data) => axios.put('/api/buisness', data),
+  updateBusiness: (data) => axios.put("/api/buisness", data),
+
   // !!! USE WITH EXTREME CAUTION !!!
-  nukeBusiness: () => axios.delete('/api/buisness'),
+  nukeBusiness: () => axios.delete("/api/buisness"),
 
   // **REVIEW API CALLS**
   createReview: (id) => axios.post(`/api/review/${id}`),
-  findBusinessReviews: (id) => axios.get(`/api/review/buisness/${id}`) 
-}
+  findBusinessReviews: (id) => axios.get(`/api/review/buisness/${id}`),
+};
 
-export default API
+export default API;
