9b8f95727021daa98fb5e8cd07d30c792ea39bbb
David Viramontes <dviramontes29@yahoo.com>
2020-09-28 10:14:46 -0700

    card

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'components/FontawesomeIcons';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Importing Pages
--- client/src/components/BuisnessCard/BuisnessCard.js
+++ client/src/components/BuisnessCard/BuisnessCard.js
@@ -1,33 +1,40 @@
 import React from 'react'
+import './BusinessCard.css'
+import Logos from '../assets/logos.png'
+import {} from @fortawes
+// import Font from 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
+
+// import StarRatings from './react-star-ratings';
 
 const BuisnessCard = () => {
   return (
-    <div className="card">
-    <div className="card-image"></div>
-    <div class="card-text">
-        <h2>business</h2>
-        <span className="date">location</span>
-        <h4>star rating</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit laudantium voluptates dolore omnis expedita reprehenderit voluptatibus sint ex officiis. Ipsum beatae itaque similique ipsam!</p>
+    <div className='profile-card'>
+      <div className='card-header'>
+        <div className='pic'>
+          <img src={Logos} alt='Portrait' />
+        </div>
+        <div className='name'>Banda CashMax</div>
+        <div className='desc'>Authentic Mexican music for all your family to enjoy</div>
+        <div className='stars'>
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+          <i className='fas fa-star' />
+        </div>
+        <a href='#' className='contact-btn'>Parti Tyme</a>
+      </div>
+      <div className='card-footer'>
+        <div className='buttons'>
+          <ul>
+            <li><a href='#'><i className='fa fa-facebook' aria-hidden='true' /></a></li>
+            <li><a href='#'><i className='fa fa-instagram' aria-hidden='true' /></a></li>
+            <li><a href='#'><i className='fa fa-id-card-o' aria-hidden='true' /></a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-    <div className="card-stats">
-        <div class="stat">
-            <div className="value">IG</div>
-        </div>
-
-        <div className="stat">
-            <div class="value">fb</div>
-        </div>
-        
-         <div className="stat">
-            <div clasName="type">link</div>
-        </div>
-    </div>
-
-</div>
-</div>
-)
+  )
 }
 
 export default BuisnessCard
