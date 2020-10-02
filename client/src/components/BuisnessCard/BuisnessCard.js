34eab7c8b0a3481aefed6441dd94b13a42e66874
David Viramontes <dviramontes29@yahoo.com>
2020-10-02 03:34:45 -0700

    cards are looking good

--- client/src/components/BuisnessCard/BuisnessCard.js
+++ client/src/components/BuisnessCard/BuisnessCard.js
@@ -3,35 +3,34 @@ import './BusinessCard.css'
 import StarRating from '../StarRating/StarRating.js'
 import Logos from '../assets/images/logos.png'
 import { FaFacebookF, FaInstagram, FaPortrait } from 'react-icons/fa'
-// import API from '../../utils/API'
-
-// import Font from 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
-
-// import StarRatings from './react-star-ratings';
 
 const BuisnessCard = props => {
 
   return (
-    <div className='profile-card'>
-      <div className='card-header'>
-        <div className='pic'>
-          <img src={props.business.img} alt='Portrait' />
-        </div>
-        <div className='name'>Banda CashMax</div>
-        <div className='desc'>Authentic Mexican music for all your family to enjoy</div>
-        <StarRating />
-        <a href='#' className='contact-btn'>Parti Tyme</a>
+    <div className='Profile-Card'>
 
+      <div className='header'>
+        <img className='profile-image' src={Logos} alt='profile pic' />
+        <div className='title'>DJ CashMax</div>
       </div>
-      <div className='card-footer'>
+
+      <div className='profile-main'>
+        <a href='#' className='contact-btn'>Parti Tyme</a>
+        <div className='desc'>
+        Modest DJ for all types of fun. You can book me for anything in
+        between a friendly family party and the rager that will last all night.
+        </div>
         <div className='buttons'>
           <ul>
-            <li className='icon'><a href={props.business.facebook}><FaFacebookF size='2em' color='#3b5998' /></a></li>
-            <li><a href={props.business.instagram}><FaInstagram size='2em' color='#bc2a8d' /></a></li>
-            <li><a href={props.business.website}><FaPortrait size='2em' color='rgb(192, 70, 70)' /></a></li>
+            <li className='icon'><a href={props.business.facebook}><FaFacebookF size='1.4em' color='#3b5998' /></a></li>
+            <li><a href={props.business.instagram}><FaInstagram size='1.4em' color='#bc2a8d' /></a></li>
+            <li><a href={props.business.website}><FaPortrait size='1.4em' color='rgb(192, 70, 70)' /></a></li>
           </ul>
         </div>
+
+        <div className='star'><StarRating /></div>
       </div>
+
     </div>
   )
 }
--- client/src/components/StarRating/StarRating.js
+++ client/src/components/StarRating/StarRating.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './StarRating.css'
 import { FaStar } from 'react-icons/fa'
 
 const StarRating = () => {
