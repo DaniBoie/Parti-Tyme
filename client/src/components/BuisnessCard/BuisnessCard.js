cb0c9430ad3548cc10449915ac0df3df8f7f0954
Bao Nguyen <467bnguyen@gmail.com>
2020-10-02 19:29:52 -0700

    Pushing toward deployment

--- client/src/components/BuisnessCard/BuisnessCard.js
+++ client/src/components/BuisnessCard/BuisnessCard.js
@@ -1,33 +1,58 @@
-import React from 'react'
-import './BusinessCard.css'
-import StarRating from '../StarRating/StarRating.js'
-import Logos from '../assets/images/logos.png'
-import { FaFacebookF, FaInstagram, FaPortrait } from 'react-icons/fa'
+import React from "react";
+import "./BusinessCard.css";
+import StarRating from "../StarRating/StarRating.js";
+import Logos from "../assets/images/logos.png";
+import { FaFacebookF, FaInstagram, FaPortrait } from "react-icons/fa";
 
 const BuisnessCard = (props) => {
-  return (
-    <div className='Profile-Card'>
+  function handleContactButton() {
+    localStorage.setItem("pickBusiness", `${props.business._id}`);
+  }
 
-      <div className='header'>
-        <img className='profile-image' src={props.business.img} alt='profile pic' />
-        <div className='title'>{props.business.name}</div>
+  return (
+    <div className="Profile-Card">
+      <div className="header">
+        <img
+          className="profile-image"
+          src={props.business.img}
+          alt="profile pic"
+        />
+        <div className="title">{props.business.name}</div>
       </div>
 
-      <div className='profile-main'>
-        <a href='#' className='contact-btn'>Parti Tyme</a>
-        <div className='desc'>{props.business.descprition}
-        </div>
-        <div className='buttons'>
+      <div className="profile-main">
+        <a
+          href="/businessprofile"
+          className="contact-btn"
+          onClick={handleContactButton}
+        >
+          Parti Tyme
+        </a>
+        <div className="desc">{props.business.bio}</div>
+        <div className="buttons">
           <ul>
-            <li className='icon'><a href={props.business.facebook}><FaFacebookF size='1.4em' color='#3b5998' /></a></li>
-            <li><a href={props.business.instagram}><FaInstagram size='1.4em' color='#bc2a8d' /></a></li>
-            <li><a href={props.business.website}><FaPortrait size='1.4em' color='rgb(192, 70, 70)' /></a></li>
+            <li className="icon">
+              <a href={props.business.facebook}>
+                <FaFacebookF size="1.4em" color="#3b5998" />
+              </a>
+            </li>
+            <li>
+              <a href={props.business.instagram}>
+                <FaInstagram size="1.4em" color="#bc2a8d" />
+              </a>
+            </li>
+            <li>
+              <a href={props.business.website}>
+                <FaPortrait size="1.4em" color="rgb(192, 70, 70)" />
+              </a>
+            </li>
           </ul>
         </div>
 
-        <div className='Stars'><StarRating /></div>
+        <div className="Stars">
+          <StarRating />
+        </div>
       </div>
-
     </div>
   );
 };
--- client/src/utils/API/API.js
+++ client/src/utils/API/API.js
@@ -5,45 +5,52 @@ const API = {
 
   // Send Frontend data as object
   registerUser: (data) => axios.post(`/api/users/register`, data),
-  loginUser: (data) => axios.post('/api/users/login', data),
-  getUser: () => 
-    axios.get('/api/users/me', {
+  loginUser: (data) => axios.post("/api/users/login", data),
+  getUser: () =>
+    axios.get("/api/users/me", {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('user')}`
-      }
+        Authorization: `Bearer ${localStorage.getItem("user")}`,
+      },
     }),
 
   // **USER PROFILE API CALLS**
-  createSettings: (data, headers) => axios.post(`/api/settings`, data, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('user')}`
-    }
-  } ),
-  updateSettings: (data) => axios.put("/api/settings", data, {
+  createSettings: (data, headers) =>
+    axios.post(`/api/settings`, data, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem('user')}`
-      }
+        Authorization: `Bearer ${localStorage.getItem("user")}`,
+      },
+    }),
+  updateSettings: (data) =>
+    axios.put("/api/settings", data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("user")}`,
+      },
     }),
 
   // **BUISNESS API CALLS**
   createBusiness: (data) => axios.post(`/api/buisness`, data),
-  updateBusiness: (data) => axios.put("/api/buisness", data, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('user')}`
-    }
-  }),
+  updateBusiness: (data) =>
+    axios.put("/api/buisness", data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("user")}`,
+      },
+    }),
   searchBusinessName: (searchTerm) =>
     axios.get(`/api/buisness/filter/${searchTerm}`),
   searchBusinessCategory: (category) =>
     axios.get(`/api/buisness/search/${category}`),
-  getAllBusiness: () => axios.get('/api/buisness'),
-  getOneBusiness: (id) => 
-    axios.get(`/api/business/${id}`),
+  getAllBusiness: () => axios.get("/api/buisness"),
+  getOneBusiness: (id) => axios.get(`/api/buisness/${id}`),
   // !!! USE WITH EXTREME CAUTION !!!
   nukeBusiness: () => axios.delete("/api/buisness"),
 
   // **REVIEW API CALLS**
-  createReview: (id) => axios.post(`/api/review/${id}`),
+  createReview: (data) =>
+    axios.post(`/api/review`, data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("user")}`,
+      },
+    }),
   findBusinessReviews: (id) => axios.get(`/api/review/buisness/${id}`),
 
   // **Filtering Algorithms**
@@ -53,19 +60,22 @@ const API = {
   },
 
   filterSubcategory: (subCategory, oldArray) => {
-    let newArray = oldArray.filter( (business) => business.subCategory === subCategory);
+    let newArray = oldArray.filter(
+      (business) => business.subCategory === subCategory
+    );
     return newArray;
   },
 
   filterCategory: (category, oldArray) => {
-    if(category === 'All'){
-      return oldArray
+    if (category === "All") {
+      return oldArray;
     } else {
-    let newArray = oldArray.filter( (business) => business.buisness_type === category );
-    return newArray
+      let newArray = oldArray.filter(
+        (business) => business.buisness_type === category
+      );
+      return newArray;
     }
-  }
-
+  },
 };
 
 export default API;
